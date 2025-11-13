ffbcb47456cc7eed76369343503a866e4bf4af7f
Ronald-Wesh <ronaldwaweru8@gmail.com>
2025-11-13 15:49:45 +0300

    done with Review.js

---
+++ Server/Models/Review.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+
+const ReviewSchema=new mongoose.Schema({
+    reviewer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    building:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Building",
+        required:true
+    },
+    ratings:{
+        security:{type:[Number,"Rate From scale of 1 to 5 "],min:1,max:5},
+        water:{type:[Number,"Rate from scale of 1 to 5"],min:1,max:5},
+        landlord:{type:[Number,"Rate from scale of 1 to 5"],min:1,max:5}
+    },
+    isAnonymous:{tpe:Boolean,
+        default:true
+    },
+    comment:{type:String}
+},{timestamps:true});
+
+module.exports=mongoose.model("Review",ReviewSchema)
