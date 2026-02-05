b9db258cfafb34fed180da717174560b5b27978f
Ronald-Wesh <ronaldwaweru8@gmail.com>
2026-02-05 20:30:13 +0300

    fix: correct variable casing and improve post creation response handling

--- Server/Controllers/forumController.js
+++ Server/Controllers/forumController.js
@@ -1,4 +1,4 @@
-const forumPost=require('../Models/ForumPost');
+const ForumPost=require('../Models/ForumPost');
 
 //Create a new forum post
 exports.createPost = async (req,res) => {
@@ -7,9 +7,19 @@ exports.createPost = async (req,res) => {
       building: req.params.buildingId,
       post: req.body.post
     });
-
-    res.status(201).json(post);
+    const saved=await post.save();
+    res.status(201).json(saved);
+    // res.status(201).json(post);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
+
+//Get All forums of a specific building
+exports.getAllForums=async(req,res)=>{
+  try{
+
+  }catch(err){
+    
+  }
+}
