c185e7911c9dbdcdf8833bbf6db2df848b0124ab
Ronald-Wesh <ronaldwaweru8@gmail.com>
2026-02-06 06:59:43 +0300

    feat: enhance forum functionality with specific building retrieval and post deletion

--- Server/Controllers/forumController.js
+++ Server/Controllers/forumController.js
@@ -17,10 +17,37 @@ exports.createPost = async (req,res) => {
 
 //Get All forums of a specific building
 exports.getAllForums=async(req,res)=>{
-  try {
+  try {//populate-replaces the buikding and user id with the actual building and user data
     const posts = await ForumPost.find().populate('user', 'username').populate('building');
     res.status(200).json(posts);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 }
+
+// .find({ ... })
+// Finds all posts that match the given condition.
+exports.getSpecificBuildingForums=async(req,res)=>{
+  try{
+    const buildingPosts=await ForumPost.find({building:req.params.buildingId})
+    .populate('user',"username")
+    .sort({createdAt:-1});
+    res.status(200).json(buildingPosts)
+  }catch(err){
+    res.status(400).json({message:err.message});
+  }
+};
+
+exports.deletePost=async(req,res)=>{
+  try{
+    const post=await ForumPost.findById(req.params.postId);
+    if(!post)return res.status(404).json({message:'Post Not Found'});
+    if(post.user.toString()!==req.user._id.toString()){
+      return res.status(403).json({message:"Not Authrized"});
+    }
+    await post.deleteOne();
+    res.json({message:'Post Deleted Successfully'});
+  }catch(err){
+    res.status(500).json({message:err.message});
+  }
+};
