f5ce5b646c8beea3cf4f227dc675e0e7053e8247
Ronald-Wesh <ronaldwaweru8@gmail.com>
2026-02-24 23:58:09 +0300

    feat: add verification controller and authentication middleware to verifier routes

--- Server/Routes/verRoutes.js
+++ Server/Routes/verRoutes.js
@@ -1,3 +1,6 @@
 //Used by admin/verifier to view requests & verify landlords
 const express = require('express');
 const router = express.Router();
+const verificationController = require('../controllers/verificationController');
+const { protect} = require('../middleware/authMiddleware');
+const isVerifier=require("../middleware/verifier")
