f861fb215e2caa44c722ab7ec91d0bdb8237c354
Ronald-Wesh <ronaldwaweru8@gmail.com>
2026-03-21 16:54:38 +0300

    feat: Implement Clerk authentication and establish React Router with role-based protected routes.

---
+++ Client/src/Utils/protectedRoute.jsx
@@ -0,0 +1,78 @@
+//Who is allowed to see which pages
+import React from 'react';
+import {Navigate} from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+//component that protects routes
+export const ProtectedRoute=({children})=>{
+  const {isAuthenticated,loading}=useAuth();//getting auth data
+  if(loading){
+    return(
+      <div className='min-h-screen flex items-center justify-center'>
+        <div className='flex flex-col items-center'>
+          <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-primary'></div>
+          <p className='mt-4 text-gray-600'>Loading...</p>
+        </div>
+      </div>
+    )
+  }
+  //if not logged in redirect to login page
+  if(!isAuthenticated){
+    return <Navigate to="/login" replace/>
+  }
+  //if logged in allow access to the page
+  return children;
+};
+
+//Role protected Routes
+export const RoleProtectedRoute=({children,allowedRoles=[],redirectTo="/listings"})=>{
+  const {user,loading,isAuthenticated}=useAuth();
+  if(loading){
+    return(
+      <div className='min-h-screen flex items-center justify-center'>
+        <div className='flex flex-col items-center'>
+          <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-primary'></div>
+          <p className='mt-4 text-gray-600'>Checking permissions...</p>
+        </div>
+      </div>
+    )
+  }
+  //if not logged in redirect to login page
+  if(!isAuthenticated){
+    return <Navigate to="/login" replace/>
+  }
+  //if logged in but not allowed role redirect to login page
+  if(allowedRoles.length>0 && !allowedRoles.includes(user?.role)){
+    return <Navigate to={redirectTo} replace/>
+  }
+  //if logged in and allowed role allow access to the page
+  return children;
+}
+
+//ADMIN PROTECTED ROUTES
+export const AdminRoute=({children})=>{
+  return(
+    <RoleProtectedRoute allowedRoles={['admin']}>
+      {children}
+    </RoleProtectedRoute>
+  );
+};
+
+//LANDLORD PROTECTED ROUTES
+export const LandlordRoute=({children})=>{
+  return(
+    <RoleProtectedRoute allowedRoles={['landlord']}>
+      {children}
+    </RoleProtectedRoute>
+  );
+};
+
+export const TenantRoute=({children})=>{
+  return(
+    <RoleProtectedRoute allowedRoles={['tenant']}>
+      {children}
+    </RoleProtectedRoute>
+  );
+};
+
+
