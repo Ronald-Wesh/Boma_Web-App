df5f09c06c432cd445a5213d37324629e837f9dd
Ronald-Wesh <ronaldwaweru8@gmail.com>
2026-03-24 09:04:24 +0300

    feat: Migrate toast notifications from react-hot-toast to sonner and enhance login/registration error messages.

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import {ClerkProvider} from '@clerk/clerk-react'
 import {BrowserRouter} from 'react-router-dom'
+import { AuthProvider } from './context/authContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
     <BrowserRouter>
+    <AuthProvider>
     <App />
+    </AuthProvider>
     </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
